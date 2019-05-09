0488520fdf7b31e6d2ce14b2a1a6895a35779034
Justin Vo <justinvo1998@gmail.com>
2019-05-08 21:20:33 -0400

    Implemented map and location services
    The map will center on user location and follow the user.
    
    Location services are turned on in the Map tab and the Posts tab.

--- RadiUs/RadiUs/MapViewController.swift
+++ RadiUs/RadiUs/MapViewController.swift
@@ -13,13 +13,18 @@ import FirebaseDatabase
 import Firebase
 import CoreData
 import CoreLocation
+import MapKit
 
 /*
  This View Controller controls the Map tab (the one that displays the map).
  */
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     var databaseRef:DatabaseReference!
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     let msGreen = UIColor(rgb: 0x00FA9A)
     let limeGreen = UIColor(rgb: 0x90EE90)
@@ -30,7 +35,56 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Map: viewDidLoad() called")
+        
+        // Set up Firebase
         databaseRef = Database.database().reference()
+        
+        // Set up visuals
         self.view.backgroundColor = aquamarine
+        
+        // Set up location
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if CLLocationManager.authorizationStatus() == .authorizedAlways ||
+            CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Permission granted, so start tracking")
+            locationManager.startUpdatingLocation()
+        } else {
+            print("Trying to request permission...")
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.requestLocation()
+        
+        // Set up map
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print ("error: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print ("Update location from MAP")
+        centerMap(onLocation: locations.last!.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways ||
+            CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    /*
+     Centers the map on a given 2D corrdinate. This function is used whenever the location is updated.
+     */
+    func centerMap(onLocation loc: CLLocationCoordinate2D) {
+        let radius: CLLocationDistance = 1000
+        let region = MKCoordinateRegion(center: loc, latitudinalMeters: radius, longitudinalMeters: radius)
+        mapView.setRegion(region, animated: true)
     }
 }
--- RadiUs/RadiUs/PostsViewController.swift
+++ RadiUs/RadiUs/PostsViewController.swift
@@ -17,9 +17,12 @@ import CoreLocation
 /*
  This View Controller controls the view that holds all of the nearby posts (the center tab).
  */
-class PostsViewController: UIViewController {
+class PostsViewController: UIViewController, CLLocationManagerDelegate {
     
     var databaseRef:DatabaseReference!
+    
+    var locationManager = CLLocationManager()
+    var previousLocation: CLLocation?
     
     let msGreen = UIColor(rgb: 0x00FA9A)
     let limeGreen = UIColor(rgb: 0x90EE90)
@@ -29,13 +32,50 @@ class PostsViewController: UIViewController {
     
     @IBOutlet weak var newPostOutlet: UIButton!
     
+    /*
+     Because the Login segues to this ViewController, this is the first ever viewDidLoad()
+     called upon logging in.
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Posts: viewDidLoad() called")
+        
+        // Set up Firebase
         databaseRef = Database.database().reference()
+        
+        // Set up visuals
         self.view.backgroundColor = aquamarine
         newPostOutlet.backgroundColor = limeGreen
         newPostOutlet.roundCorners(corners: [.topLeft, .topRight], radius: 40)
         newPostOutlet.setTitleColor(loginText, for: .normal)
+        
+        // Set up location services
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
+        if CLLocationManager.authorizationStatus() == .authorizedAlways ||
+            CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Permission granted, so start tracking")
+            locationManager.startUpdatingLocation()
+        } else {
+            print("Trying to request permission...")
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print ("error: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print ("Update loc from POSTS")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways ||
+            CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
     }
 }
