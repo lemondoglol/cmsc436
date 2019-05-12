e7ad9014728e0d4bf3a4e0043947d9607ccc3c73
Justin Vo <justinvo1998@gmail.com>
2019-05-12 14:41:08 -0400

    Clicking map annotation sends to the post.
    Can now see comments on a post.
    
    Can now actually make a post.
    
    Default sorting based on distance.

--- RadiUs/RadiUs/PostAndRepliesViewController.swift
+++ RadiUs/RadiUs/PostAndRepliesViewController.swift
@@ -18,6 +18,82 @@ import CoreLocation
  This class represents the view that shows the original post and all of its replies.
  A user can see this view when he clicks on the post on the map or from the Posts tab.
  */
-class PostAndRepliesViewController: UIViewController {
+class PostAndRepliesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var databaseRef:DatabaseReference!
+    
+    var post: Post?
+    var replies: [String]?
+    
+    @IBOutlet weak var tableViewOutlet: UITableView! {
+        didSet {
+            tableViewOutlet.dataSource = self
+            tableViewOutlet.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var postLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        replies = post?.comments
+        postLabel.text = post?.content
+        
+        // Set up Firebase
+        databaseRef = Database.database().reference()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return replies!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "ReplyCell", for: indexPath)
+        if let myCell = cell as? ReplyCell {
+            
+            myCell.replyLabelOutlet.text = replies![indexPath.row]
+        }
+        return cell
+    }
+    
+    /*
+     Disables the tapping of replies / comments.
+     TODO: remove this if we want to make it so tapping a comment does something. Maybe show the full comment?
+     TODO: go to attribute selector in storyboard and selection style -> None
+     */
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func makeCommentsOnPost(comment: String, postID: String){
+        var post:Post!
+        databaseRef.child("postTable").observeSingleEvent(of: .value, with: {(snapshot) in
+            for child in snapshot.children.allObjects as! [DataSnapshot] {
+                let postkeyID = child.key
+                if postkeyID != postID {
+                    continue
+                }
+                let content = child.childSnapshot(forPath: "content").value as? String
+                let latitude = child.childSnapshot(forPath: "latitude").value as? Double
+                let longitude = child.childSnapshot(forPath: "longitude").value as? Double
+                var comments = child.childSnapshot(forPath: "comments").value as? [String]
+                let res = Post(postID: postkeyID, content: content!, latitude: latitude!, longitude: longitude!)
+                post = res
+                if comments == nil {
+                    comments = [String]()
+                }
+                post.comments = comments!
+                break
+            }
+            post!.comments.append(comment)
+            self.databaseRef.child("postTable").child(post.postID).child("content").setValue(post.content)
+            self.databaseRef.child("postTable").child(post.postID).child("latitude").setValue(post.latitude)
+            self.databaseRef.child("postTable").child(post.postID).child("longitude").setValue(post.longitude)
+            self.databaseRef.child("postTable").child(post.postID).child("comments").setValue(post.comments)
+        })
+    }
 }
