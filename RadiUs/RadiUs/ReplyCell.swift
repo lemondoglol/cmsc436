9d0af1e58262b20f369c2231c597557c616f1067
Justin Vo <justinvo1998@gmail.com>
2019-05-12 14:57:43 -0400

    Fixed bugs and added files

---
+++ RadiUs/RadiUs/ReplyCell.swift
@@ -0,0 +1,25 @@
+//
+//  ReplyCell.swift
+//  RadiUs
+//
+//  Created by Justin Vo on 5/11/19.
+//  Copyright © 2019 lemondog. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Foundation
+import FirebaseDatabase
+import Firebase
+import CoreData
+import CoreLocation
+
+/*
+ Similar to the Post cell, but this shows the replies instead.
+ */
+class ReplyCell: UITableViewCell {
+    
+    // TODO: connect outlet to storyboard
+    @IBOutlet weak var replyLabelOutlet: UILabel!
+    
+}
